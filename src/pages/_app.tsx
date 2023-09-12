33094b5cf85f288fd57a744ec8cbdc73b1ddfd7b
premstaller1 <premstaller-spam@outlook.de>
2023-09-12 23:43:37 +0200

    Got Mantine to run with next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import HeaderAction from '../components/header'; // Adjust the path as needed
+
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-
+  const links = [
+    {
+      link: '/',
+      label: 'Home',
+      links: [ // Provide an array of sub-links within the 'links' property
+        { link: '/subpage1', label: 'Subpage 1' },
+        { link: '/subpage2', label: 'Subpage 2' },
+      ],
+    },
+    // Add more top-level links as needed
+  ];
   return (
     <>
       <Head>
@@ -15,6 +27,10 @@ export default function App(props: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+      <div>
+      <HeaderAction links={links} />
+      {/* Rest of your page content */}
+      </div>
 
       <MantineProvider
         withGlobalStyles
